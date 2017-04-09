2057abf083fa7d183280989c5113237763ab85a3
David Hernandez <ddhz2010@yahoo.com>
2017-04-09 14:25:06 -0700

    Initial commit.

---
+++ LearningGit/src/com/mycompany/sayhello/SayHello.java
@@ -0,0 +1,15 @@
+package com.mycompany.sayhello;
+
+public class SayHello {
+
+	public static void main(String[] args) {
+		String [] friends = {"John", "Carlos", "Janet"};
+		
+		// say hi to my friends
+		for (String friend : friends) {
+			System.out.println("Hi " +  friend + "!");
+		}
+
+	}
+
+}
