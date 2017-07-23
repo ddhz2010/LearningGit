991efdb8ff94e1fb38abf85f348f2fcfe2711c26
David Hernandez <ddhz2010@yahoo.com>
2017-07-22 23:25:53 -0700

    Adding br05_d to friends list.

--- LearningGit/src/com/mycompany/sayhello/SayHello.java
+++ LearningGit/src/com/mycompany/sayhello/SayHello.java
@@ -3,7 +3,7 @@ package com.mycompany.sayhello;
 public class SayHello {
 
 	public static void main(String[] args) {
-		String [] friends = {"John", "Carlos", "Janet", "Atif, br05_b", "br05c"};
+		String [] friends = {"John", "Carlos", "Janet", "Atif, br05_b", "br05_c", "br05_d"};
 		
 		// say hi to my friends
 		for (String friend : friends) {
